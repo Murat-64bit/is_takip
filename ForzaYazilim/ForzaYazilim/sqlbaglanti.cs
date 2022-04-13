7f1e8967ba358689072265a77979d6365058e9ec
Murat-64bit <70372296+Murat-64bit@users.noreply.github.com>
2022-04-13 21:41:20 +0300

    Update sqlbaglanti.cs

--- ForzaYazilim/ForzaYazilim/sqlbaglanti.cs
+++ ForzaYazilim/ForzaYazilim/sqlbaglanti.cs
@@ -11,7 +11,7 @@ namespace ForzaYazilim
     {
         public SqlConnection baglanti()
         {
-            SqlConnection baglan = new SqlConnection(@"Data Source=91.151.93.29;Initial Catalog=DboForzaMetal;User Id=kisitli; password=:z#sKiYo}BB24rYyEYOY*DdDhV7P;");
+            SqlConnection baglan = new SqlConnection();
             baglan.Open();
             return baglan;
         }
